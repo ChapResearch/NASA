e04b34659baf108a92c854a436ce884711e746e9
Eric Rothfus <eric@rothfus.com>
2019-01-23 09:31:09 -0600

    added change monitoring to numSpinner

--- Website/Contributor/numSpinner.js
+++ Website/Contributor/numSpinner.js
@@ -18,39 +18,37 @@
 //    but keeps the input's value up-to-date so that querying its value
 //    works as expected.
 //
-
-//
-// numSpinner_init() - paints the HTML components for the numSpinner.
-//                     The numSpinner is always attached to a regular
-//                     <input type="number"> so that jQuery reference
-//                     must be passed in. It's position, width, height
-//                     is assumed to be configured already.
-//
-function numSpinner_init(target)
-{
-    // turn off the display of the target itself
-
-    target.hide();
-
-    // now add the appropraite html to target's parent
-
-    target.parent().append('<br><div class="numSpinner_minus numSpinner_field">&ndash;</div><div class="numSpinner_value numSpinner_field">0</div><div class="numSpinner_plus numSpinner_field">+</div>');
-}
+//    The orginal number value can be changed, too, but the jQuery
+//    trigger('change') must be called so that this simulation can wake-up
+//    to the change.
 
 function numSpinner_initAll()
 {
+
+    // first, add the simulation
+    
     $('input[type="number"]').each(function() {
 	var target = $(this);                    // this is the associated <input> field
-	var value = parseInt(target.val());
 	var parent = target.parent();
 
-	numSpinner_init(target);
+	target.hide();                    // hide the number field itself
+
+	target.parent().append('<br><div class="numSpinner_minus numSpinner_field">&ndash;</div>');
+	target.parent().append('<div class="numSpinner_value numSpinner_field">0</div>');
+	target.parent().append('<div class="numSpinner_plus numSpinner_field">+</div>');
+    });
+
+    // then add the clicks on the simulated buttons
+    
+    $('input[type="number"]').parent().each(function() {
+	var parent = $(this);
+	var target = parent.find('input');
 
 	var spinnerValue = parent.find('.numSpinner_value');
 	var spinnerPlus =  parent.find('.numSpinner_plus');
 	var spinnerMinus =  parent.find('.numSpinner_minus');
 
-	spinnerValue.html(value);
+	spinnerValue.html(parseInt(target.val()));	// set the initial value for the spinner
 
 	spinnerMinus.click(function() {
 	    var value = parseInt(spinnerValue.html());
@@ -71,8 +69,23 @@ function numSpinner_initAll()
 		target.val(value);
 	    }
 	});
-
     });
+
+    // finally, monitor the hidden field for value changes, which can only be made by
+    //  javascript, so the jQuery.trigger('change') must be called when changing the value
+
+    // this is done in a zero setTimeout() so that it occurs in the next UI loop so that
+    //  jQuery can catch-up with the changes to DOM that occurred with the overlay of
+    //  the number spinners.
+    
+    setTimeout(function() {
+	$('input[type="number"]').change(function() {
+	    var target = $(this);
+	    var parent = target.parent();
+	    var spinnerValue = parent.find('.numSpinner_value');
+	    spinnerValue.html(target.val());
+	});
+    },0);
 }
 
 	
