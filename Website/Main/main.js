30579e88ed848541ca748b05224dc92c3211b08f
Eric Rothfus <eric@rothfus.com>
2019-01-25 17:24:51 -0600

    added basic main page

---
+++ Website/Main/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+    $('div.webpage').click(function() {
+	window.location = "Contributor";
+    });
+
+    $('div.statistics').click(function() {
+	window.location = "Statistics";
+    });
+
+    $('div.app').click(function() {
+	window.location = "App";
+    });
+
+    $('div.about').click(function() {
+	window.location = "About";
+    });
+
+});
+    
+			   
