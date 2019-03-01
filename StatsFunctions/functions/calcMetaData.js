99c39ae9d96536fbbe22c9834ca894f81d2d7a6f
Eric Rothfus <eric@rothfus.com>
2019-03-01 10:35:28 -0600

    added contains and weight and vaious fixes

--- StatsFunctions/functions/calcMetaData.js
+++ StatsFunctions/functions/calcMetaData.js
@@ -6,9 +6,11 @@
 
 var combineDelta = require('./combineDelta');
 
+var METADATA = "_metaData";
 
 module.exports = {
-    calcMetaData: function (data,params,xmldata) { return(calcMetaData(data,params,xmldata));}
+    calcMetaData: function (data,params,xmldata) { return(calcMetaData(data,params,xmldata));},
+    calcMetaDataUpper: function (data,metaData,ref) { return(calcMetaDataUpper(data,metaData,ref));}
 };
 
 
@@ -19,21 +21,122 @@ module.exports = {
 //                  it when done.  "params" has the year/robot/competion/match
 //                  object.
 //
-function calcMetaData(data,params,xmldata)
+function calcMetaData(data,params,metaData)
 {
-    // the xmldata object includes a "metaData" property that is an ordered array
-    //   of new fields that need to be created, based upon previous data.
-
-    var metaData = xmldata.metaData;
-
     for(var i=0; i < metaData.length; i++) {
-	console.log("Processing: " + metaData[i].name);
-	console.log(metaData[i]);
+//	console.log("Processing: " + metaData[i].name);
+//	console.log(metaData[i]);
 	data[metaData[i].name] = calcMetaDataField(data,params,metaData[i]);
      
     }
 
     return(data);
+}
+
+//
+// calcMetaDataUpper() - calculate the metaData for all perspectives BUT the
+//                       match.  Incoming data is the ENTIRE DATABASE. After
+//                       changes, the entire database is returned to be written
+//                       back.
+//
+//                       This routine USED to send back the changed database, and
+//                       the whole thing would be slammed back into the database.
+//                       but this caused a cascade of updates for matches. So now
+//                       the changes are done directly given the "ref".
+//
+function calcMetaDataUpper(data,metaDataSpec,ref)
+{
+    // now, cruise through the data updating things...
+
+    for(var year in data) {
+	if(year != METADATA) {
+	    for(var robot in data[year]) {
+		if(robot != METADATA) {
+		    for(var competition in data[year][robot]) {
+			if(competition != METADATA) {
+			    data[year][robot][competition][METADATA] =
+				upperMetaData(metaDataSpec,"competition",data[year][robot][competition]);
+			    console.log(data[year][robot][competition][METADATA]);
+			    ref.child(year).child(robot).child(competition).child(METADATA)
+				.set(data[year][robot][competition][METADATA]);
+			}
+		    }
+		    data[year][robot][METADATA] = upperMetaData(metaDataSpec,"robot",data[year][robot]);
+		    ref.child(year).child(robot).child(METADATA).set(data[year][robot][METADATA]);
+		}
+	    }
+	    data[year][METADATA] = upperMetaData(metaDataSpec,"year",data[year]);
+	    ref.child(year).child(METADATA).set(data[year][METADATA]);
+	}
+    }
+
+    // this may not be a thing...but what the heck
+    data[METADATA] = upperMetaData(metaDataSpec,"top",data);
+    ref.child(METADATA).set(data[METADATA]);
+
+    return(data);
+}
+
+//
+// upperMetaData() - calculate the upper metaData (that above match). The new metaData is returned.
+//                   The perspective is "year", "robot", or "competition", and the data needs to be
+//                   the data object for that thing at that perspective, like:
+//
+//                        data[year][robot][competition]
+//
+function upperMetaData(metaDataSpec,perspective,data)
+{
+    var localData = {};            // used to accumulate the "input" data for the metaData calculations
+    var newMetaData = {};          // all new metaData is stored here for later return
+
+    // first, get all of the data for all lower layers
+
+    for(var lowerLevel in data) {
+	if(lowerLevel != METADATA) {               // skip this level's METADATA
+
+	    // if the lower level has METADATA use it
+	    if(data[lowerLevel].hasOwnProperty(METADATA)) {
+		dataMerge(localData,data[lowerLevel][METADATA]);
+	    }
+
+	    // if we are a the competition level, use ALL lower-level match data
+	    if(perspective == "competition") {
+		dataMerge(localData,data[lowerLevel]);
+	    }
+	}
+    }
+
+    // and then process it all, if there is anything to do
+
+    if(metaDataSpec.hasOwnProperty(perspective)) {
+	for(var i=0; i < metaDataSpec[perspective].length; i++) {
+	    var metaTarget = metaDataSpec[perspective][i];
+//	    console.log("Processing: " + metaTarget.name);
+	    var meta = calcMetaDataField(localData,{},metaTarget);
+	    localData[metaTarget.name] = meta;
+	    newMetaData[metaTarget.name] = meta;
+	}
+    }
+
+    return(newMetaData);
+}
+
+//
+// dataMerge() - given a target object, and an additional object, merge
+//               them together, returning the target object with the other
+//               merged in.
+//
+function dataMerge(target,additional)
+{
+    for(var add in additional) {
+	if(target.hasOwnProperty(add)) {
+	    target[add].push(additional[add]);
+	} else {
+	    target[add] = [additional[add]];
+	}
+    }
+
+    return(target);
 }
 
 //
@@ -56,6 +159,8 @@ function calcMetaDataField(data,params,field)
     case 'delta':    return(calcMetaDataField_delta(data,params,field));
     case 'sum':      return(calcMetaDataField_sum(data,params,field));
     case 'percent':  return(calcMetaDataField_percent(data,params,field));
+    case 'weight':   return(calcMetaDataField_weight(data,params,field));
+    case 'contains': return(calcMetaDataField_contains(data,params,field));
     default:         console.log("BAD OP: " + operation); return(false);
     }
 
@@ -69,22 +174,158 @@ function calcMetaDataField(data,params,field)
 //                                    well above where the data is updated.
 //
 
-function calcMetaDataField_count(data,params,field)
+//
+// _weight() - given target target(s) with targetVal(s), compute a weighted
+//             sum of the target(s). Each target is multiplied by the targetVal
+//             and then added to the sum. The sum is the value of the field.
+//
+function calcMetaDataField_weight(data,params,field)
 {
-    var target = field.target;           // name of the data field to count
+    var targets = field.target;
+    var multipliers = field.multiplier;
+    var lows = field.low;
+    var highs = field.high;
 
-    if(data.hasOwnProperty(target)) {    // make sure the target field exists
-	if(typeof data.target === 'object' && data.target !== null) {
-	    var count = 0;
-	    for(k in data.target) {
-		count++;
-	    }
-	    return(count);
-	}
-	return(1);
+    // targets/targetVals should always be arrays, but just in case...
+
+    if (!Array.isArray(targets)) { targets = [targets]; }
+    if (!Array.isArray(multipliers)) { multipliers = [multipliers]; }
+    if (!Array.isArray(lows)) { lows = [lows]; }
+    if (!Array.isArray(highs)) { highs = [highs]; }
+
+    // process each of the targets summing the output
+    //   The targets are our driver - if there are more multipliers/lows/highs
+    //   than targets, the remaining multipliers/lows/highs are ignored. If there
+    //   are more targets than the multipliers/lows/highs, the missing:
+    //   multipliers are assumed to be 1, the min is 0, and max 1.
+
+    for(var i=0; i < (targets.length - multipliers.length); i++) {
+	multipliers.push(1);
+    }
+    for(var i=0; i < (targets.length - lows.length); i++) {
+	lows.push(0);
+    }
+    for(var i=0; i < (targets.length - highs.length); i++) {
+	highs.push(1);
     }
 
-    return(0);
+    // IMPORTANT NOTE - this code doesn't support targets being an
+    //                  ARRAY of ARRAYS - so you can't do a "weight"
+    //                  op above the match level...it will fail.
+
+    // note that if a target doesn't exist in the data, then it is
+    // quietly ignored
+
+    var sum = 0;
+    for(var i=0; i < targets.length; i++) {
+	var item = normalizeDataItem(data,targets[i]);
+
+	if(item.length >= 1) {
+
+	    var targetValue = Number.parseFloat(item[0]);            // use the first value only
+	    var multiplier = Number.parseFloat(multipliers[i]);
+	    var high = Number.parseFloat(highs[i]);
+	    var low = Number.parseFloat(lows[i]);
+	    var inverted = false;
+
+	    if(Number.isNaN(targetValue)) {
+		console.log("Bad value for " + targets[i]);
+		continue;     // skip bad values or multipliers
+	    }
+	    if(Number.isNaN(multiplier)) {
+		console.log("Bad multiplier for " + targets[i] + "(" + multiplier[i] + ")");
+		continue;
+	    }
+	    if(Number.isNaN(high)) {
+		console.log("Bad high for " + targets[i] + "(" + highs[i] + ")");
+		continue;
+	    }
+	    if(Number.isNaN(low)) {
+		console.log("Bad low for " + targets[i] + "(" + lows[i] + ")");
+		continue;
+	    }
+
+	    if(high < low) {     // if the high is smaller, then we are "inverted"
+		var temp = low;  // flip the values to be numerically high/low
+		low = high;
+		high = temp;
+		inverted = true;
+	    }
+
+	    var range = high-low;
+	    var offsetValue = targetValue-low;
+	    var normalValue = offsetValue/range;
+
+	    if(inverted) {
+		sum += (1-normalValue)*multiplier;
+	    } else {
+		sum += normalValue*multiplier;
+	    }
+	}
+    }
+
+    return(sum);
+}
+
+//
+// normalizeDataItem() - Given a particular data item (from Firebase), normalize it
+//                        into an array of values. This deals with things like
+//                        hasOwnProperty() and null values.
+//
+//     RETURNS - an array is ALWAYS returned - if the data item had one item, then the array
+//               has one item - if the field doesn't exist or is null, then an empty array
+//               is returned.
+//
+function normalizeDataItem(data,fieldName)
+{
+    var returnArray = [];      // empty array returned for no field or null value
+
+    // make sure the target field exists and is non-null
+
+    if(data.hasOwnProperty(fieldName) && data[fieldName] !== null) {
+
+	// check to see if it is an object (multiple values)
+	//  (normally you'd need to check for null, because null also has typeof 'object'
+	//    but we already checked for null above)
+	
+	if(typeof data[fieldName] === 'object') {
+	    for(var prop in data[fieldName]) {
+		if(data[fieldName].hasOwnProperty(prop)) {
+		    returnArray.push(data[fieldName][prop]);
+		}
+	    }
+	} else {
+	    returnArray.push(data[fieldName]);    	    // single value
+	}
+    }
+    return(returnArray);
+}
+
+function calcMetaDataField_count(data,params,field)
+{
+    var targets = field.target;           // name of the data field to count
+
+    if (!Array.isArray(targets)) {
+	targets = [targets];
+    }
+
+    var count = 0;
+
+    for(var i=0; i < targets.length; i++) {
+	if(data.hasOwnProperty(targets[i])) {    // make sure the target field exists
+	    if(typeof data[targets[i]] === 'object' && data[targets[i]] !== null) {
+		for(var prop in data[targets[i]]) {
+		    if(data[targets[i]].hasOwnProperty(prop)) {
+			count++;
+		    }
+		}
+	    } else {
+		count++;
+	    }
+	}
+    }
+
+    return(count);
 }
 
 function calcMetaDataField_sum(data,params,field)
@@ -136,20 +377,32 @@ function calcMetaDataField_combine(data,params,field)
 
 	    if(data.hasOwnProperty(ourTargets[i])) {    // make sure the target field exists
 		if(typeof data[ourTargets[i]] === 'object' && data[ourTargets[i]] !== null) {
-		    console.log(data[ourTargets[i]]);
 		    x = combineDelta.combineOP(x,data[ourTargets[i]]);
 		}
 	    }
     }
-    console.log(x);
+
     return(x);
 }
 
 function calcMetaDataField_delta(data,params,field)
 {
-    var start = field.start;           // name of the data field to count
-    var end = field.end;
-    var delta = combineDelta.deltaOP(data[start],data[end]);
+    var start = field.start[0];    // only use the first field of start/end
+    var end = field.end[0];
+
+    // if the start or end fields aren't there, default them to empty
+
+    startVal = [];
+    if(data.hasOwnProperty(start)) {
+	startVal = data[start];
+    }
+
+    endVal = [];
+    if(data.hasOwnProperty(end)) {
+	endVal = data[end];
+    }
+           
+    var delta = combineDelta.deltaOP(startVal,endVal);
     return(delta);
 
 
@@ -168,7 +421,6 @@ function calcMetaDataField_average(data,params,field)
     }
 
     var sum = 0.0;
-    var avg = 0.0;
     var count = 0.0;
 
     for (var i = 0; i<ourTargets.length; i++) {
@@ -177,52 +429,79 @@ function calcMetaDataField_average(data,params,field)
 	    if(typeof data[ourTargets[i]] === 'object' && data[ourTargets[i]] !== null) {
 		for(k in data[ourTargets[i]]) {
 		    if(data[ourTargets[i]].hasOwnProperty(k)) {
-			sum += parseFloat(data[ourTargets[i]][k]);
-			count++;
+			if(data[ourTargets[i]][k] !== null) {
+			    sum += parseFloat(data[ourTargets[i]][k]);
+			    count++;
+			}
 		    }
 		}
 	    } else {
-		sum += parseFloat(data[ourTargets[i]]);
-		count++;
-	    }
-	}
-    }	
-
-	
-    avg = sum/count;
-
-    return(avg);
-
-}
-
-function calcMetaDataField_percent(data,params,field)
-{
-    var target = field.target;           // name of the data field to count
-
-    var ourTargets;
-
-    if (Array.isArray(target)) {
-	ourTargets = target;
-    } else {
-	ourTargets = [target];
-    }
-
-    var count = 0.0;
-    var percent = 0;
-
-    if(data.hasOwnProperty(ourTargets)) {    // make sure the target field exists
-	console.log(field);
-
-	if(typeof data.target === 'object' && data.target !== null) {
-	    for(var i=0;i<ourTargets.length;i++) {
-		if (parseFloat(data[ourTargets[i]]) == parseFloat(field.targetVal)) {
+		if(data[ourTargets[i]] !== null) {
+		    sum += parseFloat(data[ourTargets[i]]);
 		    count++;
 		}
 	    }
 	}
+    }	
+
+    // we have to protect ourselves here, if there is NO data, then count is zero
+    //   and this causes avg to be NaN (can't divide by zero).  If this is the case,
+    //   simply return null.
+
+    if(count == 0) {
+	return(null);
+    } else {
+	return(sum/count);
     }
-    console.log(count);
-    console.log(ourTargets.length);
-    percent = (count/ourTargets.length) * 100;
+}
+
+//
+// _percent() - calculate the percentage of targets that have the given targetVal
+//              in the data. Note that the comparison to the target/targetVal is
+//              done WITHOUT parsing - that is, as a string.
+//              You CAN NOT have multiple target values. A single one only.
+//
+function calcMetaDataField_percent(data,params,field)
+{
+    // only use the first target & targetVal
+
+    var target = field.target[0];          // these are always arrays coming in
+    var targetVal = field.targetVal[0];
+
+    var dataTarget = normalizeDataItem(data,target);
+    var percent = 0;
+
+    if(dataTarget.length) {
+	var count = 0;
+	for(var i=0; i < dataTarget.length; i++) {
+	    if(dataTarget[i] == targetVal) {
+		count++;
+	    }
+	}
+	percent = (count/dataTarget.length) * 100;
+    }
+
     return(percent);
+}
+
+//
+// _contains() - used to see if a data element (array) contains a particular
+//               value. Returns 1 if so, 0 otherwise. If there are multiple values
+//               all must be contained for the contains to be true.  Comparison is
+//               done by string.
+//
+function calcMetaDataField_contains(data,params,field)
+{
+    var target = field.target[0];          // these are always arrays coming in
+    var targetVals = field.targetVal;
+
+    var dataTarget = normalizeDataItem(data,target);
+
+    for(var i=0; i < targetVals.length; i++) {
+	if(!dataTarget.includes(targetVals[i])) {
+	    return(0);       // if ANY don't match, then we don't match
+	}
+    }
+
+    return(1);
 }
